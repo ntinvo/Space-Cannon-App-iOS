51c96e84b3fa64d93c2860d580ee9e8fd43d65e5
tinvo1101 <tinnvo1101@gmail.com>
2015-09-06 23:07:46 -0500

    init

---
+++ Space Cannon/Space Cannon/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Space Cannon
+//
+//  Created by Tin N Vo on 9/6/15.
+//  Copyright © 2015 Tin Vo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Space Cannon/Space Cannon/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Space Cannon
+//
+//  Created by Tin N Vo on 9/6/15.
+//  Copyright © 2015 Tin Vo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
